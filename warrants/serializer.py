98461108cc16e03fa0ad1aa90f9b5df9bd06e121
Denim MacDougall <dmacdougall7987@conestogac.on.ca>
2026-01-26 17:06:41 -0500

    created views

--- warrants/serializer.py
+++ warrants/serializer.py
@@ -3,32 +3,35 @@ from .models import *
 # Create your models here.
 
 # Auto incrementing primary keys are created by default
-class CrimeSerializer(models.Model):
+class CrimeSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Crime
         fields = ['section_number', 'description']
   
-class CitizenSerializer(models.Model):
 
-    class Meta:
-        model = Citizen
-        fields = ['first_name', 'last_name', 'race', 'sex', 'age', 'details']
-
-class License_PlateSerializer(models.Model):
+class License_PlateSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = License_Plate
         fields = ['plate_number', 'owner', 'car_model', 'car_make']
 
-class OfficerSerializer(models.Model):
+class OfficerSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Officer
         fields = ['badge_number', 'citizen_id', 'is_staff', 'is_active']
 
-class WarrantSerializer(models.Model):
+class WarrantSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Warrant
         fields = ['crime_number', 'citizen_involved', 'approving_judge']
+
+class CitizenSerializer(serializers.HyperlinkedModelSerializer):
+
+    warrants = WarrantSerializer(source = 'warrant_set', many = True, read_only = True)
+    vehicles = License_PlateSerializer(source = 'license_plate_set', many = True, read_only = True)
+    class Meta:
+        model = Citizen
+        fields = ['first_name', 'last_name', 'warrants', 'vehicles', 'race', 'sex', 'age', 'details']
