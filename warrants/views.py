a0d7a5a435031af77cf51021a2e4bd51b50500a8
Denim MacDougall <dmacdougall7987@conestogac.on.ca>
2026-01-26 14:11:38 -0500

    created outline for api endpoints

--- warrants/views.py
+++ warrants/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def Warrant(request):
+
+    if request.method == "GET":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "POST":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "PATCH":
+        return JsonResponse({"status" : "ok"})
+    
+    else:
+        return JsonResponse({"Error" : "Request method not allowed"}, status=405)
+
+def Crime(request):
+
+    if request.method == "GET":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "POST":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "PATCH":
+        return JsonResponse({"status" : "ok"})
+    
+    else:
+        return JsonResponse({"Error" : "Request method not allowed"}, status=405)
+    
+
+def Citizen(request):
+    if request.method == "GET":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "POST":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "PATCH":
+        return JsonResponse({"status" : "ok"})
+    
+    else:
+        return JsonResponse({"Error" : "Request method not allowed"}, status=405)
+    
+
+def Vehicle(request):
+    if request.method == "GET":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "POST":
+        return JsonResponse({"status" : "ok"})
+    
+    elif request.method == "PATCH":
+        return JsonResponse({"status" : "ok"})
+    
+    else:
+        return JsonResponse({"Error" : "Request method not allowed"}, status=405)
+
+
